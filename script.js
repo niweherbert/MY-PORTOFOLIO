72455c730ebb2309471c79df4bd53576da5268bb
Niwe Herbert <ntihemukaherbert@gmail.com>
2025-03-15 14:45:53 +0200

    Frontend

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Fullstack Developer . . ." , "Programmer . . ." , "Web Developer . . .", "UI UX Designer . . .", "DevOps developer . . ."],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
